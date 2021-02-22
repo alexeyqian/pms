05b05cf61a947e3bd49644e14669b86debf133a9
Alexey Qian <v-xiaqian@microsoft.com>
2021-02-22 22:36:11 +0700

    Update bug index table

--- Controllers/SyncController.cs
+++ Controllers/SyncController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.CodeAnalysis;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Options;
 using Newtonsoft.Json;
 using PMS.Data;
@@ -17,14 +18,16 @@ using System.Web;
 namespace PMS.Controllers
 {
     public class SyncController : Controller
-    {        
+    {
+        private readonly IConfiguration _configuration;
         private readonly PMSDBContext _context;       
         private readonly string[] _ids;
 
-        public SyncController(PMSDBContext context)
+        public SyncController(IConfiguration configuration, PMSDBContext context)
         {
+            _configuration = configuration;
             _context = context;
-            //_ids = System.IO.File.ReadAllLines("bugids.txt");
+            _ids = System.IO.File.ReadAllLines("bugids.txt");
         }
 
         public async Task<IActionResult> Index()
@@ -34,6 +37,27 @@ namespace PMS.Controllers
             //await syncHelper.SyncBug(bugId);
 
             return View();
+        }
+
+        public async Task<string> SyncBatch(bool syncExisting = false)
+        {
+            var syncHelper = new SyncHelper(_configuration, _context);
+
+            foreach (var id in _ids)
+            {            
+                var bug = await _context.Bug.Where(r => r.NO == int.Parse(id)).FirstOrDefaultAsync();
+                if (bug == null) // create new record
+                {
+                    await syncHelper.SyncBug(int.Parse(id));
+                }
+                else // data already exist
+                {
+                    if (syncExisting)
+                        await syncHelper.SyncBug(int.Parse(id));
+                }
+            }
+
+            return "OK";
         }
 
     }
